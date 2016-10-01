ea14dcae9a1af8047da3345ef7e263a9634f61e4
locustking <daryl.r.greer@gmail.com>
2016-10-01 12:33:15 -0700

    mob part 1

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,54 +8,43 @@ class GildedRose {
     }
 
     public void updateQuality() {
-//        for (int i = 0; i < items.length; i++) {
         for(Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
+
+            if (item.name.contains("Sulfuras")) {
+                item.quality = 80;
+            } else if(item.name.equals("Aged Brie") && item.quality < 50 && item.sellIn<0){
                     item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
+            } else {
+                if (!item.name.equals("Aged Brie") &&
+                        !item.name.contains("Backstage passes") &&
+                        item.quality > 0) {
+                            item.quality = item.quality - 1;
                 } else {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
+
+                        if (item.name.contains("Backstage passes")) {
+                            if (item.sellIn < 11) {
+                                item.quality = item.quality + 1;
+                            }
+
+                            if (item.sellIn < 6) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
                     }
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                        if (!item.name.contains("Backstage passes") && item.quality > 0) {
+
+                                item.quality = item.quality - 1;
+
+                        } else {
+                            item.quality = item.quality - item.quality;
+                        }
                 }
             }
         }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -53,7 +53,7 @@ public class GildedRoseTest {
         app.updateQuality();
         assertEquals("Foo", app.items[0].name);
         assertEquals(46, app.items[0].quality);
-        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(-2, app.items[0].sellIn);
     }
 
     @Test
@@ -142,7 +142,17 @@ public class GildedRoseTest {
 //        assertEquals(80, app.items[2].quality);
     }
 
+    @Test
+    public void testToString(){
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 11, 80),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Sulfuras, Hand of Ragnaros", 10, 80)
 
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros, 11, 80", app.items[0].toString());
+    }
 
 
 }
